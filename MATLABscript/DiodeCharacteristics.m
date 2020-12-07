2e3270a65bbd4442fd7412891750df9b6578edf8
Deepthy Shajan <Deepthy.Shajan@kpit.com>
2020-12-07 19:38:32 +0530

    Modification in saturation current value from 1e-12 to 1e-9.

--- MATLABscript/DiodeCharacteristics.m
+++ MATLABscript/DiodeCharacteristics.m
@@ -1,5 +1,5 @@
 v_t=0.0258; %Thermal voltage
-i_saturation = 1.0e-12; % Saturation Current
+i_saturation = 1.0e-9; % Saturation Current
 v_diode =0.45:0.01:1; % Voltage Sampling; % Voltage Sampling
 n=1.9; %Emission coefficient
 i = i_saturation*(exp(v_diode/(n*v_t))-1); % Diode Current
