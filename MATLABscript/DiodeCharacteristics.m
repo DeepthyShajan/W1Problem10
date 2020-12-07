6bae28e90fd06ed5aa4e40f61115e8dd02cfac8c
Deepthy Shajan <Deepthy.Shajan@kpit.com>
2020-12-07 20:47:20 +0530

    A knob connected to adjust diode voltage and obtained  corresponding diode current in display.

--- MATLABscript/DiodeCharacteristics.m
+++ MATLABscript/DiodeCharacteristics.m
@@ -1,6 +1,6 @@
 v_t=0.0258; %Thermal voltage
 i_saturation = 1.0e-9; % Saturation Current
-v_diode =0.45:0.01:1; % Voltage Sampling; % Voltage Sampling
+v_diode =0.45:0.01:1; % Voltage Sampling; 
 n=1.9; %Emission coefficient
 i = i_saturation*(exp(v_diode/(n*v_t))-1); % Diode Current
 %% Plotting the Results
